eee8b96cef81dc6fe16d7be7c363323b85d0438d
Simply Done <olko4455@gmail.com>
2023-12-15 22:49:29 +0200

    second commit

--- HW_24/script.js
+++ HW_24/script.js
@@ -1,13 +1,13 @@
-let catagories = ['Літні кросівки','Зимові кросівки', 'Осінні кросівки'];
+let catagories = ['Літні кросівки', 'Зимові кросівки', 'Осінні кросівки'];
 
 let products = {
-    'Літні кросівки': ['NewBalanceSummer','NikeSmellWinter','NewBalanceHotSneakers'],
-    'Зимові кросівки': ['NikeSmellWinter','NewBalanceWinter','NewBalanceColdSneakers'],
-    'Осінні кросівки': ['OldFashionSneakers','NewOneSneakers','NiceChoiceSneakers'],
+    'Літні кросівки': ['NewBalanceSummer', 'NikeSmellWinter', 'NewBalanceHotSneakers'],
+    'Зимові кросівки': ['NikeSmellWinter', 'NewBalanceWinter', 'NewBalanceColdSneakers'],
+    'Осінні кросівки': ['OldFashionSneakers', 'NewOneSneakers', 'NiceChoiceSneakers'],
 }
 
 let productsDetailes = {
-    'NewBalanceSummer': 'Good choice for the every men who like to run in the morning!', 
+    'NewBalanceSummer': 'Good choice for the every men who like to run in the morning!',
     'NikeSmellWinter': 'Good choice for the everyone who likes to go for a long walk',
     'NewBalanceHotSneakers': 'Good choice if you want to wear comfortable shoes for every day',
     'NewBalanceHotSneakers': 'Good choice if you want to wear comfortable shoes for every day!',
@@ -18,32 +18,32 @@ let productsDetailes = {
     'NiceChoiceSneakers': 'Good choice if you want to boast in front of your friends about your shoes',
 }
 
-let mainContainer = document.getElementById(`main-container`);
+let mainContainer = document.getElementById('main-container');
 
-mainContainer.style.display = `flex`;
-mainContainer.style.flexDirection = `row-reversed`;
+mainContainer.style.display = 'flex';
+mainContainer.style.flexDirection = 'row-reversed';
 
 let activeButton = null;
 
 function displayCatagories() {
-    let leftBlock = document.getElementById(`left-block`);
-    leftBlock.style.display = `flex`;
-    leftBlock.style.gap = `15px`;
-    leftBlock.style.margin = `12px`;
-    leftBlock.style.flexDirection = `column`;
-    leftBlock.style.border = `4px solid black`
-    while(leftBlock.firstChild) {
+    let leftBlock = document.getElementById('left-block');
+    leftBlock.style.display = 'flex';
+    leftBlock.style.gap = '15px';
+    leftBlock.style.margin = '12px';
+    leftBlock.style.flexDirection = 'column';
+    leftBlock.style.border = '4px solid black';
+    while (leftBlock.firstChild) {
         leftBlock.removeChild(leftBlock.firstChild)
     }
     catagories.forEach(function(catagory) {
-        let button = document.createElement(`button`);
+        let button = document.createElement('button');
         button.textContent = catagory;
         button.style = 'padding: 20px; background-color: white; font-size: 22px;font-weight: 700; border: 0px;'
         button.onclick = function() {
-            if(activeButton) {
+            if (activeButton) {
                 activeButton.style.backgroundColor = 'white';
             }
-            this.style.backgroundColor = `blue`;
+            this.style.backgroundColor = 'blue';
             activeButton = this;
             displayProducts(catagory);
         };
@@ -52,20 +52,20 @@ function displayCatagories() {
 }
 
 function displayProducts(catagory) {
-    let middleBlock = document.getElementById(`middle-block`);
-    middleBlock.style.margin = `12px`;
-    while(middleBlock.firstChild) {
+    let middleBlock = document.getElementById('middle-block');
+    middleBlock.style.margin = '12px';
+    while (middleBlock.firstChild) {
         middleBlock.removeChild(middleBlock.firstChild);
     }
     products[catagory].forEach(function(product) {
-        let button = document.createElement(`button`);
+        let button = document.createElement('button');
         button.textContent = product;
-        button.style = 'margin: 10px; border: 0px; font-size: 22px; font-weight: 700; display: flex; flex-direction: column; border: 2px solid black'
+        button.style = 'margin: 10px; border: 0px; font-size: 22px; font-weight: 700; display: flex; flex-direction: column; border: 2px solid black';
         button.onclick = function() {
-            if(activeButton) {
+            if (activeButton) {
                 activeButton.style.backgroundColor = 'white';
             }
-            this.style.backgroundColor = `grey`;
+            this.style.backgroundColor = 'grey';
             activeButton = this;
             displayProductDetails(product);
         }
@@ -74,37 +74,38 @@ function displayProducts(catagory) {
 }
 
 function displayProductDetails(product) {
-    let rightBlock = document.getElementById(`right-block`);
-    while(rightBlock.firstChild) {
+    let rightBlock = document.getElementById('right-block');
+    while (rightBlock.firstChild) {
         rightBlock.removeChild(rightBlock.firstChild);
     }
-    let productDetail = document.createElement(`p`);
+    let productDetail = document.createElement('p');
     productDetail.style.margin = '12px'
     productDetail.textContent = productsDetailes[product];
     rightBlock.appendChild(productDetail);
-    let button = document.createElement(`button`);
-    button.textContent = `Купити`;
+    let button = document.createElement('button');
+    button.textContent = 'Купити';
     button.style = 'background-color: green; border-radius: 6px; border: 0px; margin: 12px; padding: 12px; color:white'
     button.onclick = function() {
         let messageBox = document.getElementById('message-box');
-        messageBox.style.position = 'absolute'; 
+        messageBox.style.position = 'absolute';
         messageBox.style.top = '300px';
-        messageBox.textContent = `Товар ${product} куплено!`;
+        messageBox.textContent = 'Товар ${product} куплено!';
         messageBox.style.display = 'block';
         messageBox.style.fontSize = '22px'
-        setTimeout(function(){
-            messageBox.style.display = 'none';},3000);
+        setTimeout(function() {
+            messageBox.style.display = 'none';
+        }, 3000);
         displayCatagories();
-        
-        while(rightBlock.firstChild) {
+
+        while (rightBlock.firstChild) {
             rightBlock.removeChild(rightBlock.firstChild);
         }
-        let middleBlock = document.getElementById(`middle-block`);
-        while(middleBlock.firstChild) {
+        let middleBlock = document.getElementById('middle-block');
+        while (middleBlock.firstChild) {
             middleBlock.removeChild(middleBlock.firstChild);
         }
     }
     rightBlock.appendChild(button);
 }
 
-window.addEventListener(`load`,displayCatagories);
+window.addEventListener('load', displayCatagories);
