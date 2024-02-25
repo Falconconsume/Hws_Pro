0d4e1530272fe7dc58f82bafcdddc95617352a47
Simply Done <olko4455@gmail.com>
2024-02-25 09:24:34 +0200

    first commit

---
+++ hw_35/src/AppWrapper.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "../src/store/store";
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import ColorModeContext from "./components/theme/ColorModeContext";
+import { getDesignTokens } from "./components/theme/pallets";
+
+export default function AppWrapper() {
+  const [mode, setMode] = React.useState("light");
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+      },
+    }),
+    []
+  );
+
+  const theme = React.useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <CssBaseline />
+          <App />
+        </Provider>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
